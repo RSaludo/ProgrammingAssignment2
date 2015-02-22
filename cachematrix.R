851cd90aeedf3280cac3d900fa346552f358a653
RSaludo <rienne.saludo@gmail.com>
2015-02-22 18:14:14 -0500

    This is the Feb22 commit for Answer2. Previous versions should be ignored.

--- cachematrix.R
+++ cachematrix.R
@@ -1,9 +1,12 @@
-## Line 6 starts the makeCacheMatrix function that stores the output of the cacheSolve 
-## function from the variable i. The vector container must be created first.
-## Line 24 starts the cacheSolve function that provides the inverse of the given matrix (x)
+## This is the 2nd attempt at this assignment branched to Feb22-ProgrammingAssignment2.
+## Line 9 starts the makeCacheMatrix function that stores the output of the cacheSolve
+## function frim the variable 'i'.
+## Line 26 starts the cachSolve function that provides the inverse of the given matrix (x).
 
-## I changed the vaiables from the original mean cache to solve and substituted i for m.
-makeCacheMatrix <- function(x = matrix()) {
+## I changed the variables from the original mean cache to solve and substituted 'i' for 'm'.
+## The vector container is created first. The assignment opperator '<-' was used in lieu of '='.
+
+makeCacheMatrix <- function(x <- matrix()) {
         i <- NULL
         set <- function(y) {
                 x <<- y
@@ -11,17 +14,16 @@ makeCacheMatrix <- function(x = matrix()) {
         }
         get <- function() x
         setSolve <- function(solve) i <<- solve
-        getSolve <- function () i
-        list(set = set, get = get,
-             setSolve = setSolve,
-             getSolve = getSolve)
+        getsolve <- function () i
+        list(set <- set, get <- get,
+             setSolve <- setSolve,
+             getSolve <- getSolve)
 }
 
-## Similar to above, I changed out the general mean function to solve and made sure
-## the variables matched the container above.
+## Similar to above, I used solve in lieu of mean and '<-' in lieu of '='.
+## This function will return a matrix that is the inverse of 'x'.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
         i <- x$getSolve()
         if(!is.null(i)) {
                 message("getting cached data")
